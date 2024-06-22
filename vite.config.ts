e4841a9b4e0aa2ec804b9c1f2e879d1bdf2fbb21
Niklas Higi <niklas@higi.dev>
2024-06-22 11:18:44 +0200

    Change Vite server port to avoid conflicts with other projects

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import type { UserConfig } from "vite";
 import reactPlugin from "@vitejs/plugin-react";
 
 const config: UserConfig = {
+  server: {
+    port: 5656,
+  },
   plugins: [reactPlugin()],
 };
 
