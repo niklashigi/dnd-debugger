1dadec2c19d7d21dc66859faa3dc7f2346fe8572
Niklas Higi <niklas@higi.dev>
2024-06-22 12:04:00 +0200

    Update tool description

--- src/App.tsx
+++ src/App.tsx
@@ -84,16 +84,17 @@ function App() {
             Drag & Drop Debugger
           </h1>
           <div className="mt-3 text-gray-600 text-sm leading-relaxed">
-            Building advanced behaviors on top the web's{" "}
+            Implementing drag and drop behaviors, for example using the{" "}
             <a
               href="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API"
               className="hover:underline font-semibold"
             >
               HTML Drag and Drop API
-            </a>{" "}
-            can be challenging without seeing what exact data is being attached
-            to dragged item by your app or other apps. This tool helps you craft
-            drag payloads and inspect payloads from external sources.
+            </a>
+            , can enable truly magical experiences for users. Debugging such
+            logic can be challenging though because it's hard to see what exact
+            data is being attached to dragged items. This tool helps you inspect
+            and craft drag payloads to make this easier.
           </div>
         </header>
 
